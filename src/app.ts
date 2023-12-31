11551e559811ec32f0a42e129a4159bd913feb76
Matheus Alencar <matheusalencar6942@gmail.com>
2023-12-31 15:24:43 -0300

    configurando aplicação como poo

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,41 @@
-import express from "express";
+import express, { Express, Router } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = process.env.PORT || 9000;
-const app = express();
+const port = process.env.PORT ? parseInt(process.env.PORT) : 9000;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+class App {
+  express: Express;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  constructor() {
+    this.express = express();
 
-app.listen(port, () => {
-  console.log(`Application listening on port ${port}`);
-});
+    this.express.use(cors());
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: true }));
+
+    this.configRoutes();
+
+    this.express.listen(port, () => {
+      console.log(`Application listening on port ${port}`);
+    });
+  }
+
+  private configRoutes(): void {
+    const routes = Router();
+
+    routes.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+    routes.get("/ping", (req, res) => {
+      res.json({ pong: true });
+    });
+
+    this.express.use(routes);
+  }
+}
+
+new App();
